c5392e95d62d215ecba591cd75e069f1faed2d2c
Christian Brotherson <christian.brotherson@gmail.com>
2018-03-14 17:23:22 -0600

    Added the menu options for entrees and sides"

--- bottega_diner.py
+++ bottega_diner.py
@@ -1,30 +1,49 @@
 import random
 
 breakfast_menu = {
-  
+  1: ['Pancakes': 7.49],
+  2: ['French Toast': 7.99],
+  3: ['Omlet': 6.99],
+  4: ['Country Fried Steak': 10.99]
 }
 
 breakfast_menu_sides = {
-  
+  1: ['Bacon': .99],
+  2: ['Sausage': 1.49],
+  3: ['Eggs': 1.49],
+  4: ['Hash Browns': 1.49],
 }
 
 lunch_menu = {
-  
+  1: ['Steak': 15.99],
+  2: ['Hamburger': 12.99],
+  3: ['Pasta': 11.99],
+  4: ['Ribs': 13.99]
 }
 
 lunch_menu_sides = {
-  
+  1: ['Fries': .49],
+  2: ['Salad': .20],
+  3: ['Onion Rings': 1.49],
+  4: ['Baked Potato': .99],  
 }
 
 dinner_menu = {
-  
+  1: ['Steak': 20.99],
+  2: ['Hamburger': 15.99],
+  3: ['Pasta': 16.99],
+  4: ['Ribs': 17.49]  
 }
 
 dinner_menu_sides = {
-  
+  1: ['Fries': 1.49],
+  2: ['Salad': 1.20],
+  3: ['Onion Rings': 1.99],
+  4: ['Baked Potato': 1.49],   
 }
 
 def print_menu():
+  print("~~~Bottega Diner~~~")
 
 def time_of_day():
 
