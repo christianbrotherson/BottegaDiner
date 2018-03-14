d59930649a806ebccdf6ffa2232a60fcdd1e37af
Christian Brotherson <christian.brotherson@gmail.com>
2018-03-14 17:02:54 -0600

    First commit. Created outline of methods that I think I will need to create the Diner

---
+++ bottega_diner.py
@@ -0,0 +1,51 @@
+import random
+
+breakfast_menu = {
+  
+}
+
+breakfast_menu_sides = {
+  
+}
+
+lunch_menu = {
+  
+}
+
+lunch_menu_sides = {
+  
+}
+
+dinner_menu = {
+  
+}
+
+dinner_menu_sides = {
+  
+}
+
+def print_menu():
+
+def time_of_day():
+
+def waitress_comments():
+
+def order():
+
+
+class Chef_Special():
+  def __init__():
+
+  def random_entree():
+  
+
+  def random_sides():
+
+class Bill():
+  def entree_price():
+
+  def sides_prince():
+
+def main():
+
+main()
